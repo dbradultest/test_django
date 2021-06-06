417c9b3b763fe11a87a14991d07d17fdbd0c8268
AndriiLatysh <andrii.latysh@gmail.com>
2021-06-06 11:42:24 +0000

    Django QuerySet

--- students/views.py
+++ students/views.py
@@ -1,23 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-from webargs.djangoparser import use_kwargs
-
-
+from students.models import Student
+from students.utils import format_records
 
 
 def hello(request):
     return HttpResponse('Hello')
+#
+#
+# @use_kwargs({
+#     "count": fields.Int(
+#         required=False,
+#         missing=100,
+#         validate=[validate.Range(min=1, max=999)]
+#     )},
+#     location="query"
+# )
+# def generate_students(request, count):
+#     return HttpResponse('Hello')
+
+from webargs.djangoparser import use_kwargs, use_args
+from webargs import fields
 
 
-@use_kwargs({
-    "count": fields.Int(
-        required=False,
-        missing=100,
-        validate=[validate.Range(min=1, max=999)]
+@use_args({
+    "first_name": fields.Str(
+        required=False
+    ),
+    "last_name": fields.Str(
+        required=False
+    ),
+    "birthdate": fields.Date(
+        required=False
     )},
     location="query"
 )
-def generate_students(request, count):
-    return HttpResponse('Hello')
+def get_students(request, args):
+
+    students = Student.objects.all()
+
+    for param_name, param_value in args.items():
+        students = students.filter(**{param_name: param_value})
+
+        # if param_name == 'first_name':
+        #     students = students.filter(first_name=param_value)
+        # elif param_name == 'last_name':
+        #     students = students.filter(last_name=param_value)
+
+    records = format_records(students)
+
+    return HttpResponse(records)
